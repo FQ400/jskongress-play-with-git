d083205fabbc4cd7a2b40b514572f63754dc39dc
Carsten Lamm <CarstenLamm@googlemail.com>
2017-11-14 12:20:40 +0100

    Add addition2

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const addition = (a,b) => a + b;
 console.log(addition(1,2));
 
+const addition2 = (a,b) => a + b;
+console.log(addition(4,2));
+
 const substraction = (a,b) => a - b;
 console.log(substraction(1,2));
 
